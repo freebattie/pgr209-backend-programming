2da2f79702730f8bf73b43fd6a28b7a65ad46724
bjarte <22171847+freebattie@users.noreply.github.com>
2022-09-01 09:02:19 +0200

    🔴 Erey forth year is a leap year

---
+++ src/week1/TestLeapYear.java
@@ -0,0 +1,17 @@
+package week1;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestLeapYear {
+    @Test
+    void everyFortYearIsLeapYear(){
+        assertTrue(isLeapYear(2024));
+    }
+
+    private boolean isLeapYear(int i) {
+        return false;
+    }
+}
