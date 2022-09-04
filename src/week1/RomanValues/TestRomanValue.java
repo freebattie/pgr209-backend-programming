f9b3a4b7c0699c6b4e6d7be9dfda3962bb53ce35
bjarte <22171847+freebattie@users.noreply.github.com>
2022-09-04 18:42:43 +0200

    🔵 optimized some code

--- src/week1/RomanValues/TestRomanValue.java
+++ src/week1/RomanValues/TestRomanValue.java
@@ -62,7 +62,6 @@ public class TestRomanValue {
         assert(romanNumber(444).equalsIgnoreCase("CDXLIV"));
     }
 
-
     private String romanNumber(int number) {
 
         StringBuilder romanNumber = new StringBuilder();
